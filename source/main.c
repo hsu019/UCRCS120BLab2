1f25e7d96d4aafcc6d250a04736785caf84ad727
Huapei Su <hsu019@wch136-23.cs.ucr.edu>
2020-10-15 03:48:03 -0700

    test

--- source/main.c
+++ source/main.c
@@ -16,8 +16,21 @@ int main(void) {
     /* Insert DDR and PORT initializations */
 
     /* Insert your solution below */
-    while (1) {
+    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+while(1) {
+	tmpA = PINA & 0x01;
+	if (tmpA == 0x01) {
+		tmpB = (tmpB & 0xFC) | 0x01;
+	}
+	else {
+		tmpB = (tmpB & 0xFC) | 0x02;
+	}
 
-    }
-    return 1;
+	PORTB = tmpB;
+}
+
+	return 0;
 }
